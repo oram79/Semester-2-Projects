0e985302f5df3e2fb632fa73599179246fe95f36
Logan Oram <logan.oram@keyin.com>
2024-07-22 23:34:52 -0230

    quickstart

---
+++ JavaScript/QAP3/dogfinder/src/components/header.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className='mainbox'>
+
+      </div>
+      <nav className="navbar">
+        
+        {/* Add navigation links or other header content here */}
+      
+      </nav>
+
+    </header>
+  );
+}
+
+export default Header;
