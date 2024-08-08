84746cb374d3c8b5808fa748324cf519c0904803
Logan Oram <logan.oram@keyin.com>
2024-08-08 16:21:40 -0230

    comments

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,5 @@
+// Function that will allow the webpage to create a hamburger style drop down menu in screen sizes less then 1200px
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
